4f564ceaf7778dd82196ac376be920641d44717b
Olegsander78 <onkotov@gmail.com>
2021-03-05 15:30:02 +0500

    добавил все листинги

--- Boid/Assets/Scripts/LookAtAttractor.cs
+++ Boid/Assets/Scripts/LookAtAttractor.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class LookAtAttractor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Attractor.POS);  
     }
 }
